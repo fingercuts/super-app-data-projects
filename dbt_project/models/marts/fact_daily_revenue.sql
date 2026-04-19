5f698af80942b74922463c16421a660786fa6b58
fingercuts <ad3.k249@gmail.com>
2026-04-20 05:36:27 +0700

    feat(dbt): develop fact_daily_revenue analytical marts

---
+++ dbt_project/models/marts/fact_daily_revenue.sql
@@ -0,0 +1,18 @@
+{{ config(materialized='table') }}
+
+WITH raw_transactions AS (
+    -- DuckDB natively binds exactly to local massive parquet files
+    SELECT *
+    FROM read_parquet('../data/production/transactions.parquet')
+)
+
+SELECT
+    date_trunc('day', CAST(date AS TIMESTAMP)) AS transaction_date,
+    department,
+    service_id,
+    COUNT(transaction_id) as total_tx_volume,
+    SUM(total_amount) as total_gross_revenue,
+    AVG(total_amount) as average_basket_size
+FROM raw_transactions
+GROUP BY 1, 2, 3
+ORDER BY 1 DESC, 5 DESC
